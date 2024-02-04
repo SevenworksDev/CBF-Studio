8eeb0d3d9aa4a4d48c0bbe61b5651e9f85e88c84
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2024-02-03 20:54:58 -0600

    Redirect to /login if no key

--- app.py
+++ app.py
@@ -46,16 +46,19 @@ def register():
 
 @app.route('/dashboard')
 def dashboard():
-    cbf = os.path.join(os.path.dirname(__file__), 'secret', 'bots')
-    list = []
-    for keys in os.listdir("secret/bots/"):
-        bots = os.path.join(cbf, keys)
-        if os.path.isdir(bots):
-            for name in os.listdir(bots):
-                bot = os.path.join(bots, name)
-                if os.path.isfile(bot) and name.endswith('.cbf'):
-                    list.append({'name': name[:-4]})
-    return render_template('dashboard.html', list=list)
+    if 'key' in request.cookies and request.cookies['key'] in valid_keys:
+        cbf = os.path.join(os.path.dirname(__file__), 'secret', 'bots')
+        list = []
+        for keys in os.listdir("secret/bots/"):
+            bots = os.path.join(cbf, keys)
+            if os.path.isdir(bots):
+                for name in os.listdir(bots):
+                    bot = os.path.join(bots, name)
+                    if os.path.isfile(bot) and name.endswith('.cbf'):
+                        list.append({'name': name[:-4]})
+        return render_template('dashboard.html', list=list)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/bots/<string:name>')
 def bots(name):
